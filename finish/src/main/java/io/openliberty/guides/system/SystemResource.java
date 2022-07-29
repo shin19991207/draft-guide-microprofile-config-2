c27f79d6a5741c26a92e9a517969ee8b9ab35038
Morgan Chang <shin19991207@gmail.com>
2022-07-29 16:44:34 -0400

    Initial commit

---
+++ finish/src/main/java/io/openliberty/guides/system/SystemResource.java
@@ -0,0 +1,48 @@
+// tag::copyright[]
+/*******************************************************************************
+ * Copyright (c) 2017, 2022 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
+ *******************************************************************************/
+// end::copyright[]
+package io.openliberty.guides.system;
+
+// CDI
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.ws.rs.GET;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.core.Response;
+
+// JAX-RS
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@RequestScoped
+@Path("properties")
+public class SystemResource {
+
+  // tag::config-injection[]
+  @Inject
+  SystemConfig systemConfig;
+  // end::config-injection[]
+
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getProperties() {
+    if (!systemConfig.isInMaintenance()) {
+      return Response.ok(System.getProperties()).build();
+    } else {
+      return Response.status(Response.Status.SERVICE_UNAVAILABLE)
+                     .entity("ERROR: Service is currently in maintenance. Contact: "
+                         + systemConfig.getEmail().toString())
+                     .build();
+    }
+  }
+
+}
